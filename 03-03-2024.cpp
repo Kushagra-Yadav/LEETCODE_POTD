1a216117d2f143053bad044002257b5dc87e2bf3
Kushagra Yadav <137766817+Kushagra-Yadav@users.noreply.github.com>
2024-03-04 00:02:26 +0530

    Update 03-03-2024.cpp

--- 03-03-2024.cpp
+++ 03-03-2024.cpp
@@ -8,6 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+METHOD 1:
+
+class Solution {
+public:
+ int len(ListNode *head)
+ {
+     int l=0;
+     while(head!=NULL)
+     {
+         l++;
+         head=head->next;
+     }
+     return l;
+ }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int l=len(head);
+        int from_start=l-n;
+        if(l==n)
+        return head->next;
+        int actual_move=from_start-1;ListNode *prev=head;
+        while(actual_move--)
+            {
+                prev=prev->next;
+            }
+            prev->next=prev->next->next;
+            return head;
+
+    }
+};
+
+
+METHOD-2
 class Solution {
 public:
 int len(ListNode*h)
