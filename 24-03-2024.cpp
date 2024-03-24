19ed3bd2f024905520b1b781dc21c82843a0d09e
Kushagra Yadav <137766817+Kushagra-Yadav@users.noreply.github.com>
2024-03-24 23:42:00 +0530

    24-03-2024.cpp
    287. Find the Duplicate Number

---
+++ 24-03-2024.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& n) {
+        int s=n[0];
+        int f=n[0];
+        do{
+            s=n[s];
+            f=n[n[f]];
+        }while(s!=f);
+        f=n[0];
+        while(f!=s)
+        {
+            s=n[s];
+            f=n[f];
+        }
+        return s;
+    }
+};
